c490844d5ee84ca5fff995bb6aa494efa477caf4
vidgit <vibhorjoshi05@gmail.com>
2018-08-30 23:54:47 +0530

    init
    init

---
+++ engine/main.py
@@ -0,0 +1,7 @@
+from add import Add
+import sys
+
+a = sys.argv[1]
+b = sys.argv[2]
+print (Add(a,b))
+sys.stdout.flush()
---
+++ gui/linkers/add.js
@@ -0,0 +1,20 @@
+function addNumbers(){
+    console.log("Inside add numbers")
+    var python = require("python-shell")
+    var path = require("path")
+    var a = document.getElementById("a").value
+    var b = document.getElementById("b").value
+    document.getElementById("a").value=""
+    document.getElementById("a").value=""
+
+    var options = {
+        scriptPath: path.join(__dirname, '/../engine/'),
+        args: [a,b]
+    }
+
+    var add = new python('main.py', options);
+
+    add.on('message', function (message) {
+        console.log(message);
+    })
+}
